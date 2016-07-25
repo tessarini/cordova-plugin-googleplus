49bc7f4cd0ec23af5081076237a86e38a291d3c0
tessarini <moacir@nitryx.com>
2016-07-25 15:00:08 -0300

    Teste #1 ( (reverted from commit ef95373c44688757ca7f66721af7f044f987fb13)

--- src/ios/GooglePlus.m
+++ src/ios/GooglePlus.m
@@ -42,40 +42,8 @@ static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelect
     }
 }
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url
-options:(NSDictionary<NSString*, id> *)options {
-        return [[FBSDKApplicationDelegate sharedInstance] application:app
-                                                              openURL:url
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary *)options { return [[GIDSignIn sharedInstance] handleURL:url sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey] annotation:options[UIApplicationOpenURLOptionsAnnotationKey]]; }
 
-sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-
-annotation:options[UIApplicationOpenURLOptionsAnnotationKey]]
-                || [[GIDSignIn sharedInstance] handleURL:url
-
-sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-
-annotation:options[UIApplicationOpenURLOptionsSourceApplicationKey]];
-    }
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL
-*)url sourceApplication:(NSString *)sourceApplication
-annotation:(id)annotation
-    {
-
-            return [[FBSDKApplicationDelegate sharedInstance]
-application:application
-                                                              openURL:url
-
-sourceApplication:sourceApplication
-                                                           annotation:annotation
-                     ] ||
-                    [[GIDSignIn sharedInstance] handleURL:url
-
-sourceApplication:sourceApplication
-                                                     annotation:annotation];
-       }
-
-    
 @end
 
 @implementation GooglePlus
